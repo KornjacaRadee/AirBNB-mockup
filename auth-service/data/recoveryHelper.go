eb9218bdec4aea73e3e19d3ab148fc27dd6d2456
KornjacaRadee <djuricamilorad11@gmail.com>
2023-11-27 20:53:28 +0100

    Changed mail to https instead of http

--- auth-service/data/recoveryHelper.go
+++ auth-service/data/recoveryHelper.go
@@ -57,7 +57,7 @@ func SendRecoveryEmail(toEmail, recoveryToken string) {
 	from := mail.Address{"no-reply", "passrecovery@airbnbb.com"}
 	to := mail.Address{"", toEmail}
 	subject := "Password Recovery"
-	body := "Click the following link to reset your password: http://localhost:8082/reset?token=" + recoveryToken
+	body := "Click the following link to reset your password: https://localhost/auth/reset?token=" + recoveryToken
 
 	// Connect to the SMTP server
 	auth := smtp.PlainAuth("", smtpUsername, smtpPassword, smtpServer)
