7d107b577753af7d2a0aba0c348374d6f4df3227
Srdjan <srdjannn321@gmail.com>
2023-12-01 19:29:09 +0100

    Search home component

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AccomodationService } from '../services/accomodation/accomodation.service';
 
 @Component({
@@ -6,9 +6,17 @@ import { AccomodationService } from '../services/accomodation/accomodation.servi
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
-  accommodations: any[] = [];
+export class HomeComponent {
+  title = 'frontend';
+  opened = false;
   searchTerm: string = '';
+  minGuests: number = 0;
+  maxGuests: number = 0;
+  enterPressed = false;
+  searchButtonPressed = false;
+  accommodations: any[] = [];
+  filteredAccommodations: any[] = [];
+  searchSuccess = false;
 
   constructor(private accommodationService: AccomodationService) {}
 
@@ -24,6 +32,78 @@ export class HomeComponent implements OnInit {
       (error: any) => {
         console.error('Error fetching accommodations:', error);
       }
+    );
+  }
+
+  onSearch() {
+    this.enterPressed = true;
+
+    console.log('Search Term:', this.searchTerm);
+    console.log('Min Guests:', this.minGuests);
+    console.log('Max Guests:', this.maxGuests);
+
+    this.accommodationService
+      .searchAccomodations(this.searchTerm, this.minGuests, this.maxGuests)
+      .subscribe(
+        (response) => {
+          console.log('Server Response:', response);
+
+          if (response && response.length > 0) {
+            this.accommodations = response;
+            this.filteredAccommodations = this.filterAccommodations();
+            this.searchSuccess = true;
+            console.log(
+              'Filtered Accommodations:',
+              this.filteredAccommodations
+            );
+          } else {
+            this.accommodations = [];
+            this.filteredAccommodations = [];
+            this.searchSuccess = false;
+            console.log('No accommodations found');
+          }
+        },
+        (error: any) => {
+          console.error('Error searching accommodations:', error);
+          this.accommodations = [];
+          this.filteredAccommodations = [];
+          this.searchSuccess = false;
+        }
+      );
+  }
+
+  onSearchButtonClicked() {
+    this.searchButtonPressed = true;
+    this.onSearch();
+  }
+
+  resetSearch() {
+    this.accommodations = [];
+    this.filteredAccommodations = [];
+    this.searchSuccess = false;
+    this.searchTerm = '';
+    this.minGuests = 0;
+    this.maxGuests = 0;
+  }
+
+  filterAccommodations(): any[] {
+    const filtered = this.accommodations.filter((accommodation) =>
+      this.accommodationMatchesSearch(accommodation)
+    );
+    console.log('Filtered Accommodations:', filtered); // Dodajte ovu liniju
+    return filtered;
+  }
+
+  accommodationMatchesSearch(accommodation: any): boolean {
+    return (
+      (accommodation.name
+        .toLowerCase()
+        .includes(this.searchTerm.toLowerCase()) ||
+        accommodation.location
+          .toLowerCase()
+          .includes(this.searchTerm.toLowerCase())) &&
+      accommodation.minGuestNum >= this.minGuests &&
+      accommodation.maxGuestNum <= this.maxGuests
     );
   }
 }
