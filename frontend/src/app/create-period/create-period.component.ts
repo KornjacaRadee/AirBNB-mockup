44c7f4974d4dccc01d72f1634ebd2ece3f9e9285
Srdjan <srdjannn321@gmail.com>
2024-02-06 19:30:44 +0100

    Fixed issues on front

--- frontend/src/app/create-period/create-period.component.ts
+++ frontend/src/app/create-period/create-period.component.ts
@@ -1,28 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Router,ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ReservationService } from '../services/reservation/reservation.service';
 import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-create-period',
   templateUrl: './create-period.component.html',
-  styleUrls: ['./create-period.component.css']
+  styleUrls: ['./create-period.component.css'],
 })
 export class CreatePeriodComponent implements OnInit {
-  startDate: string = "";
-  endDate: string = "";
-  accommID: string = "";
+  startDate: string = '';
+  endDate: string = '';
+  accommID: string = '';
   price: number = 0;
   isPricePerGuest: boolean = false;
+  user: string = '';
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       this.accommID = params['id'];
-
     });
+    this.user = this.authService.getUserId();
   }
-  constructor( private toastr: ToastrService,private http: HttpClient,private router: Router,private route: ActivatedRoute, private reservationService: ReservationService) {}
+  constructor(
+    private toastr: ToastrService,
+    private http: HttpClient,
+    private router: Router,
+    private route: ActivatedRoute,
+    private reservationService: ReservationService,
+    private authService: AuthService
+  ) {}
 
   createAvailabilityPeriod() {
     const startDatea = new Date(this.startDate);
@@ -34,7 +43,8 @@ export class CreatePeriodComponent implements OnInit {
       StartDate: formattedStartDate,
       EndDate: formattedEndtDate,
       Price: this.price,
-      IsPricePerGuest: this.isPricePerGuest
+      IsPricePerGuest: this.isPricePerGuest,
+      HostId: this.user,
     };
 
     this.reservationService.postAvailability(availabilityPeriod).subscribe(
